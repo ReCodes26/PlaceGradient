adb8671eecf4b084e51e669269f0bfa5cbdfd18c
First Last <170429152+ReCodes26@users.noreply.github.com>
2026-07-16 20:34:06 -0400

    Added theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div className="w-full">
-    <h1 className="text-3xl font-bold text-center">PlaceGradient</h1>
-    <img className="w-32 h-32" src="/api?color=Chocolate" alt="placeholder"  crossOrigin="anonymous" />
-    <img className="w-32 h-32" src="/api?color=Pink" alt="placeholder"  crossOrigin="anonymous" />
-    <img className="w-32 h-32" src="/api?color=CornflowerBlue&theory=mono" alt="placeholder"  crossOrigin="anonymous" />
+    <div></div>
    </div>
   );
 }
